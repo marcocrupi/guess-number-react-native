b4bfe5bace9fc317555a06925e4fa192f3c29713
marcocrupi <marcocrupi@hotmail.it>
2023-01-31 08:50:58 +0100

    add attribute like macLenght to input and do buttons style

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,20 @@
 import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
+// FARE LEZIONE 6 CAPITOLO 4
+
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      {/* Usando maxLength nel campo di input posso limitare il numero di caratteri
+      che è possibile immettere */}
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -18,12 +28,23 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#72063c",
+    backgroundColor: "#4e0329",
     borderRadius: 8,
     elevation: 4,
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
